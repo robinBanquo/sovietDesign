fac89a560d5e11b94590aaf912a66212abf93f22
robinBanquo <caramaschi@hotmail.com>
2017-04-19 17:00:47 +0200

    architecture de base

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+include './template/head.php';
+?>
+<body>
+<?php
+include './template/header.php';
+?>
+<main>
+<!-- inclure ici tout les elements du conteneur principal -->
+<h1>Coucou</h1>
+</main>
+<?php
+include './template/footer.php';
+include './template/foot.php';
+?>
+</body>
+</html>
---
+++ template/head.php
@@ -0,0 +1,10 @@
+<head>
+  <meta content="text/html; charset=utf-8" http-equiv="content-type">
+  <title>Bibliothèque petits loups</title>
+    <link rel="stylesheet" type="text/css" href="./css/index.css">
+    <link rel="stylesheet" type="text/css" href="./css/accessAdmin.css">
+    <link rel="stylesheet" type="text/css" href="./css/footer.css"/>
+    <link rel="stylesheet" type="text/css" href="./css/addBook.css"/>
+    <link rel="stylesheet" type="text/css" href="./css/header.css"/>
+    <link rel="stylesheet" type="text/css" href="./css/show.css"/>
+</head>
