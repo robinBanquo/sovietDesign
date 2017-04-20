248bb4d3f1e5f98bbfe9171b1a67c68e2900f270
robinBanquo <caramaschi@hotmail.com>
2017-04-20 10:04:13 +0200

    test class + dossier JS

---
+++ template/foot.php
@@ -0,0 +1,3 @@
+<script src="./../js/jquery-3.1.1.min.js" type="text/javascript"></script>
+<script src="./../js/index.js" type="text/javascript"></script>
+<script src="./../js/header.js" type="text/javascript"></script>
