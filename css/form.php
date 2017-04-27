8a6c4b3a38ca2d5b50de660aedaa1941456899b8
root <root@localhost.localdomain>
2017-04-27 11:58:53 +0200

    fab_footer

---
+++ css/form.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+   <head>
+     <meta charset="utf-8">
+    <title> Panel adminstrateur</title>
+    <link rel="stylesheet" href="/var/www/html/Projet Sovietdesign/sovietDesign/css/footer.css" type="text/css">
+  </head>
+  <body>
+              <form action="footer.php" method="get">
+
+                <h1>Bonjour <?php $_POST[nom] ?>, vous êtes connecté en tant qu'administrateur !
+                </h1>
+
+
+
+
+</form>
+
+  </body>
+</html>
---
+++ template/footer.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+   <head>
+     <meta charset="utf-8">
+    <title> Pied de page </title>
+    <link rel="stylesheet" href="/var/www/html/sovietDesign/template/footer.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+  </head>
+  <body><footer>
+                <section><div class="mentions">
+
+
+                    <a href="#">mentions legales</a><br />
+                                <a href="#">Foire Aux Questions</a><br />
+                                      <a href="#">Conditions générales</a><br />
+                                          <a href="#">contact</a><br />
+
+                        </div>
+
+
+                        <div class="login">
+                    <a href="#">accés administrateur</a></br>
+                    <a href="#">se deconnecter</a>
+                  </div>
+
+              </section>
+
+
+
+              <!-- <form action="form.php" method="post">
+<p>Votre nom : <input type="text" name="nom" value="renseignez votre identifiant" /></p>
+<p>Votre âge : <input type="password" name="pass" value="entrez votre mot de passe" /></p>
+<p><input type="submit" value="Connexion"></p> -->
+</form>
+       </footer>
+  </body>
+</html>
