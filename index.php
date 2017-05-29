80ba08538fea740d86f5262908621d960ab13bb1
wanahigh <fabienruault@gmail.com>
2017-05-29 15:41:11 +0200

    creation de la home

--- index.php
+++ index.php
@@ -3,10 +3,44 @@
 <?php
 include './template/head.php';
 ?>
-    <body>
- <main>
-<!-- inclure ici tout les elements du conteneur principal -->
+<body>
+<main>
 
+    <!-- inclure ici tout les elements du conteneur principal -->
+
+    <section>
+        <div id ="div"> <img id="bulle" src="img/index/nTBMGgkkc.png">
+            <img id="lapin" src="img/index/lapin.png" alt="">
+            <form action="#" method="post">
+                <input id="search" type="search" placeholder="Rechercher"></input>
+                <input id="sbmt" type="submit"></input>
+            </form>
+        </div>
+    </section >
+
+
+    <section class=""perso">
+
+    <img id="garfield" src="img/index/Garfield-et-cie.png" alt="">
+        <img id="t-rex" src="img/index/T-REX.png" alt="">
+            <img id="harry" src="img/index/Harry.Potter.full.183172.png" alt="">
+        <a href="index.php"><img id="lampe" src="img/index/lampe de face final.png" alt=""></a>
+    <img src="" alt="">
+
+    </section>
+
+
+
+    <section class="hover">
+
+    <img id="cat1" src="img/index/Cat 1.png" alt="">
+         <img id="cat2" src="img/index/cat 2.png" alt="">
+    <img id="cat3" src="img/index/cat 3.png" alt="">
+
+    </section>
+
+
+</main>
 </footer>
 <?php
 include './template/foot.php';
